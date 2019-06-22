2500eb8487169278acb7335223e784805ada34e8
alflix <jieyuanz24@gmail.com>
2019-06-22 14:49:31 +0800

    add 67-addBinary

---
+++ 67-addBinary/67-addBinary.playground/Contents.swift
@@ -0,0 +1,28 @@
+/**
+ 给定两个二进制字符串，返回他们的和（用二进制表示）。
+
+ 输入为非空字符串且只包含数字 1 和 0。
+ 
+ 示例 1:
+ 输入: a = "11", b = "1"
+ 输出: "100"
+ 
+ 示例 2:
+ 输入: a = "1010", b = "1011"
+ 输出: "10101"
+ 
+
+ 思路：
+ 
+ 时间复杂度：O(n)
+ 空间复杂度：O(1)
+ */
+
+class Solution {
+    func addBinary(_ a: String, _ b: String) -> String {
+        return ""
+    }
+}
+
+let test = Solution()
+print(test.addBinary("10", "10"))

b4fb3cce0255d07b394642779392ea03f2e26962
alflix <jieyuanz24@gmail.com>
2019-09-11 23:55:56 +0800

    add: 189

---
+++ 189-rotate-array/189-rotate-array.playground/Contents.swift
@@ -0,0 +1,51 @@
+/**
+给定一个数组，将数组中的元素向右移动 k 个位置，其中 k 是非负数。
+
+示例 1:
+
+输入: [1,2,3,4,5,6,7] 和 k = 3
+输出: [5,6,7,1,2,3,4]
+
+解释:
+向右旋转 1 步: [7,1,2,3,4,5,6]
+向右旋转 2 步: [6,7,1,2,3,4,5]
+向右旋转 3 步: [5,6,7,1,2,3,4]
+
+示例 2:
+输入: [-1,-100,3,99] 和 k = 2
+输出: [3,99,-1,-100]
+ 
+解释:
+向右旋转 1 步: [99,-1,-100,3]
+向右旋转 2 步: [3,99,-1,-100]
+
+思路：将所有元素反转。然后反转前 k 个元素，再反转后面 n-kn−k 个元素，就能得到想要的结果。
+
+假设 n=7 且 k=3 。
+
+原始数组                  : 1 2 3 4 5 6 7
+反转所有数字后             : 7 6 5 4 3 2 1
+反转前 k 个数字后          : 5 6 7 4 3 2 1
+反转后 n-k 个数字后        : 5 6 7 1 2 3 4 --> 结果
+
+时间复杂度：O(log n)
+空间复杂度：O(1)
+ */
+
+import Foundation
+
+class Solution {
+    func rotate(_ nums: inout [Int], _ k: Int) {
+        nums.reverse()
+        reverse(&nums, start: 0, end: k-1)
+        reverse(&nums, start: k, end: nums.count-1)
+    }
+
+    func reverse(_ nums: inout [Int], start: Int, end: Int) {
+        guard end > start else { return }
+        nums[start...end].reverse()
+    }
+}
+
+var num = [1,2]
+Solution().rotate(&num, 3)
